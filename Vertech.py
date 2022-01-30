92babf1531d4f181a6085ccaef8e9b400af17f88
Anna <44736708+annahuangg@users.noreply.github.com>
2022-01-29 18:55:23 -0600

    Add files via upload

--- Vertech.py
+++ Vertech.py
@@ -2,6 +2,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+from tkinter import *
+from tkinter.ttk import *
 
 bg_color = 'palegreen'
 
@@ -51,12 +56,13 @@ class Main_menu(tk.Frame):
         
         menu_play = tk.Button(self, text="Begin Learning!", font = "LARGE_FONT",
                               bg="mediumslateblue", fg="white",
-                              command=combine_funcs(lambda: controller.show_frame(Learn), plotAndCalculate))
+                              command=combine_funcs(lambda: controller.show_frame(Learn)))
         menu_play.pack(padx=5, pady=5)
         
         menu_quit = tk.Button(self, text="Quit", bg="PaleVioletRed", fg="white",
                               command=controller.destroy)
         menu_quit.pack(padx=5, pady=5)
+        
         
         def show_frame(self, cont):
         
@@ -75,113 +81,158 @@ class Learn(tk.Frame):
        
         Learn.class_and_grade_lbl = tk.Label(self, text = "", bg=bg_color)
         Learn.class_and_grade_lbl.pack(padx=5, pady=10)
+        
+        Learn_display_graph_btn = tk.Button(self, text="Display shape", bg="mediumslateblue", fg="white",
+                              command=plot(createWindow()))
+        Learn_display_graph_btn.pack(padx=5, pady=5)
    
         Learn_home_btn = tk.Button(self, text="Home", bg="mediumslateblue",
                                     fg="white",
                                     command=lambda: controller.show_frame(Main_menu))
-        Learn_home_btn.pack(padx=5, pady=10)   
-        
-def plot():
-    # Ask for file input
-    filename = input('Please enter the name of the file: ')
-    
-    # Try to open the file and execute the rest of the commands successfully
-    try:
-        with open(filename, "r") as f:
-            print("File successfully opened.")
-            # Variables
-            xList = [] # List of all x-values in coordinates
-            yList = [] # List of all y-values in coordinates
-            coords = [] # List of coordinates, saved as tuples
-            
-            # Add coordinates to lists
-            for line in f.readlines():
-                temp = line.split()
-                c = (float(temp[0]), float(temp[1]))
-                coords.append(c)
-                xList.append(float(temp[0]))
-                yList.append(float(temp[1]))
-            # If there's an uneven number of x-coordinates and y-coordinates, yield an error
-            if (len(xList) != len(yList)):
-                raise Exception('Number of x and y values do not match!')
-            # If the first and last coordinates do not match, yield and error
-            if (xList[0] != xList[-1] or yList[0] != yList[-1]):
-                raise Exception('Initial Coordinates do not match Final Coordinates!')
-    except IOError: #This error occurs if the file is not found, or failed to open.
-        print("File failed to open.")
-    except Exception as e: #This is a general error caused likely to faulty coordinates.
-        print(e)    
-            # Plot the result
-            #plot(xList, yList)
-def calculate():           
-            # Take first two elements in current list
-            areaTotal = 0
-            while coords:
-                coords.sort()
-                c1 = coords[0]
-                c2 = coords[1]
-                c3 = None
-                c4 = None
-                coords.pop(0)
-                coords.pop(0)
-                index = 0
-                for co in coords:
-                    if(co[1] == c1[1] and c3 == None):
-                        c3 = co
-                    elif(co[1] == c2[1] and c4 == None):
-                        c4 = co
-                    
-                    index+=1
-                    if(c3 != None or c4 != None):
-                        index-=1
-                        if (c3 != None and c4 != None):
-                            break
-                
-                # Find the length between selected coordinates
-                length = None
-                if (c3[0] > c4[0]):
-                    length = abs(c4[0]-c1[0])
-                else:
-                    length = abs(c3[0]-c1[0])
-                
-                # Find width between selected coordinates
-                width = abs(c2[1]-c1[1])
-                
-                # Find area of figure
-                areaCurrent = length * width
-                areaTotal += areaCurrent
-                #print(length, width, areaCurrent, areaTotal)
-                
-                # Determine which of the two coordinates extends out further (x-value).
-                # Eliminate the coordinate that comes up short.
-                if (c3[0] > c4[0]):
-                    newCoord = (c4[0], c3[1])
-                    coords.insert(index, newCoord)
-                    coords.pop(index+1)
-                    if (coords[0][1] > coords[1][1]):
-                        temp = coords[0]
-                        coords.pop(0)
-                        coords.insert(1, temp)
-                elif (c3[0] < c4[0]):
-                    newCoord = (c3[0], c4[1])
-                    coords.insert(index, newCoord)
-                    coords.pop(index+1)
-                    if (coords[0][1] > coords[1][1]):
-                        temp = coords[0]
-                        coords.pop(0)
-                        coords.insert(1, temp)
-                else:
-                    coords.remove(c3)
-                    coords.remove(c4)
-            
-            print("The total area of the figure is", areaTotal, "units squared.\n")
-    except IOError: #This error occurs if the file is not found, or failed to open.
-        print("File failed to open.")
-    except Exception as e: #This is a general error caused likely to faulty coordinates.
-        print(e)
 
-def plot(x, y):
-    plt.plot(x, y)
+        Learn_home_btn.pack(padx=5, pady=10)   
+
+def createWindow():
+    master = Tk()
+    newWindow = Toplevel(master)
+    return newWindow
+        
+def plot(parent):
+    with open("box_hard_1.txt", "r") as f:
+        
+        xList = [] # List of all x-values in coordinates
+        yList = [] # List of all y-values in coordinates
+        coords = []
+             
+             # Add coordinates to lists
+        for line in f.readlines():
+             temp = line.split()
+             c = (float(temp[0]), float(temp[1]))
+             coords.append(c)
+             xList.append(float(temp[0]))
+             yList.append(float(temp[1]))
+        fig = Figure(figsize = (5, 5),
+                    dpi = 100)
+      
+        # list of squares
+        #y = [i**2 for i in range(101)]
+        
+        # adding the subplot
+        plot1 = fig.add_subplot(111)
+      
+        # plotting the graph
+        #plot1 = fig.add_axes(xList)
+        plot1.plot(yList)
+        
+        
+        canvas = FigureCanvasTkAgg(fig, master = parent)  
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+      
+        # creating the Matplotlib toolbar
+       # toolbar = NavigationToolbar2Tk(canvas, createWindow)
+        #toolbar.update()
+      
+        # placing the toolbar on the Tkinter window
+        #canvas.get_tk_widget().pack()
+        # Ask for file input
+# =============================================================================
+#     filename = input('Please enter the name of the file: ')
+#     
+#     # Try to open the file and execute the rest of the commands successfully
+#     #try:
+#         with open(filename, "r") as f:
+#             print("File successfully opened.")
+#             # Variables
+#             xList = [] # List of all x-values in coordinates
+#             yList = [] # List of all y-values in coordinates
+#             
+#             
+#             # Add coordinates to lists
+#             for line in f.readlines():
+#                 temp = line.split()
+#                 c = (float(temp[0]), float(temp[1]))
+#                 coords.append(c)
+#                 xList.append(float(temp[0]))
+#                 yList.append(float(temp[1]))
+#             # If there's an uneven number of x-coordinates and y-coordinates, yield an error
+#             if (len(xList) != len(yList)):
+#                 raise Exception('Number of x and y values do not match!')
+#             # If the first and last coordinates do not match, yield and error
+#             if (xList[0] != xList[-1] or yList[0] != yList[-1]):
+#                 raise Exception('Initial Coordinates do not match Final Coordinates!')
+#             fig = Figure(figsize = (5, 5), dpi = 100)    
+#             plt.plot(xList, yList)
+#             canvas = FigureCanvasTkAgg(fig, master = vertech)  
+#             canvas.draw()
+#     except IOError: #This error occurs if the file is not found, or failed to open.
+#         print("File failed to open.")
+#     except Exception as e: #This is a general error caused likely to faulty coordinates.
+#         print(e)    
+# =============================================================================
+            
+            
+def calculate():           
+    # Take first two elements in current list
+    areaTotal = 0
+    while coords:
+        coords.sort()
+        c1 = coords[0]
+        c2 = coords[1]
+        c3 = None
+        c4 = None
+        coords.pop(0)
+        coords.pop(0)
+        index = 0
+        for co in coords:
+            if(co[1] == c1[1] and c3 == None):
+                c3 = co
+            elif(co[1] == c2[1] and c4 == None):
+                c4 = co
+            
+            index+=1
+            if(c3 != None or c4 != None):
+                index-=1
+                if (c3 != None and c4 != None):
+                    break
+        
+        # Find the length between selected coordinates
+        length = None
+        if (c3[0] > c4[0]):
+            length = abs(c4[0]-c1[0])
+        else:
+            length = abs(c3[0]-c1[0])
+        
+        # Find width between selected coordinates
+        width = abs(c2[1]-c1[1])
+        
+        # Find area of figure
+        areaCurrent = length * width
+        areaTotal += areaCurrent
+        #print(length, width, areaCurrent, areaTotal)
+        
+        # Determine which of the two coordinates extends out further (x-value).
+        # Eliminate the coordinate that comes up short.
+        if (c3[0] > c4[0]):
+            newCoord = (c4[0], c3[1])
+            coords.insert(index, newCoord)
+            coords.pop(index+1)
+            if (coords[0][1] > coords[1][1]):
+                temp = coords[0]
+                coords.pop(0)
+                coords.insert(1, temp)
+        elif (c3[0] < c4[0]):
+            newCoord = (c3[0], c4[1])
+            coords.insert(index, newCoord)
+            coords.pop(index+1)
+            if (coords[0][1] > coords[1][1]):
+                temp = coords[0]
+                coords.pop(0)
+                coords.insert(1, temp)
+        else:
+            coords.remove(c3)
+            coords.remove(c4)
+            
 
 def combine_funcs(*funcs):
     def combined_func(*args, **kwargs):
@@ -193,5 +244,6 @@ vertech = vertech()
 #vertech.protocol('WM_DELETE_WINDOW', overrideWindowX)
 vertech.title("Vertech")
 #vertech.geometry('+%d+%d' % (400, 200))
+coords = [] # List of coordinates, saved as tuples
 vertech.resizable(width=False, height=False)
 vertech.mainloop()
